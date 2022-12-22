dea33499c8510ffb55be9d1b697174adc167b722
SwagMuffin88 <katrilotamois@outlook.com>
2022-12-22 15:45:49 +0200

    Initial commit

---
+++ src/main/java/com/sda/QuizAndSurveyAppSdaApplication.java
@@ -0,0 +1,13 @@
+package com.sda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAndSurveyAppSdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizAndSurveyAppSdaApplication.class, args);
+    }
+
+}
